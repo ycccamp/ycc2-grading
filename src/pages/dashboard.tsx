0118acaaca1105481770816330d0c546e91baee7
pontakornth <pontakorn61@gmail.com>
2020-02-12 13:30:38 +0700

    Add dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { Heading, Text } from '@chakra-ui/core';
+import Layout from '../components/Layout';
+
+const Dashboard: NextPage = () => (
+  <Layout>
+    <Heading size="lg">แดชบอร์ด</Heading>
+    <Text>LOLLOOL</Text>
+  </Layout>
+);
+
+export default Dashboard;
