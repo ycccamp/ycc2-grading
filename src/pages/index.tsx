1733d85d7434d2402172bb35cc5b91d2fe03fee5
pontakornth <pontakorn61@gmail.com>
2020-02-12 13:31:19 +0700

    Add dashboard button to index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import { Flex, Box, Heading, Stack, Button } from '@chakra-ui/core';
+import Link from 'next/link';
 
 const Index: React.FC = () => (
   <Flex height="100vh" width="100vw" justify="center" alignItems="center" bg="pink.300" color="pink.800">
     <Box borderRadius="4px" textAlign="center" bg="white" p={16}>
       <Stack alignItems="center" spacing={6}>
         <Heading size="2xl">YCC Grading</Heading>
-        <Button size="lg" variantColor="green">
-          เข้าสู่ระบบ
-        </Button>
+        <Link href="/dashboard" passHref>
+          <Button as="a" size="lg" variantColor="green">
+            เข้าสู่ระบบ
+          </Button>
+        </Link>
       </Stack>
     </Box>
   </Flex>
