faa1208824711ac5553acf8ee15a814facc6a3e8
pontakornth <pontakorn61@gmail.com>
2020-02-12 19:29:35 +0700

    Add CandidateSelector component

---
+++ src/@types/CandidateSelectorProps.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Candidate from './Candidate';
+
+interface CandidateSelectorProps {
+  candidates: Array<Candidate>;
+  filter: any;
+}
---
+++ src/components/CandidateSelector.tsx
@@ -0,0 +1,5 @@
+import CandidateSelectorProps from '../@types/Candidate';
+
+const CandidateSelector: React.FC<CandidateSelectorProps> = () => <div>xcxx</div>;
+
+export default CandidateSelector;
