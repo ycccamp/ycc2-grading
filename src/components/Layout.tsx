213af8e58b3f8776c5502fd4c7fa8cc785414a8a
pontakornth <pontakorn61@gmail.com>
2020-02-12 13:30:13 +0700

    Add Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import { Flex, Box, Stack, Button } from '@chakra-ui/core';
+import DashbaordLink from './DashboardLink';
+
+const Layout: React.FC = ({ children }) => (
+  <Flex justify="flex-start" width="100vw" height="100vh">
+    <Box bg="gray.700" color="gray.100" width={1 / 6}>
+      <Stack spacing={4} width="100%">
+        <DashbaordLink text="Dashboard" link="/dashboard" />
+        <DashbaordLink text="คัดเลือกผู้สมัคร" link="/select" />
+        <DashbaordLink text="ให้คะแนน" link="/grading" />
+        <DashbaordLink text="ผู้ผ่านการคัดเลือก" link="/selected" />
+      </Stack>
+      <Stack mt={10} alignItems="center" width="100%">
+        <Button textAlign="center" width="90%" variantColor="red" mt="auto">
+          ออกจากระบบ
+        </Button>
+      </Stack>
+    </Box>
+    <Box width={5 / 6} p={6}>
+      {children}
+    </Box>
+  </Flex>
+);
+
+export default Layout;
