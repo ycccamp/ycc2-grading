0e9e81ecd0f96cd46a2f5dac2e0eb9b6f9684f10
pontakornth <pontakorn61@gmail.com>
2020-02-13 09:35:52 +0700

    Make candidate selector flexible

--- src/components/CandidateSelector.tsx
+++ src/components/CandidateSelector.tsx
@@ -1,6 +1,6 @@
-import { Box, BoxProps } from '@chakra-ui/core';
+import { Box, BoxProps, Button } from '@chakra-ui/core';
 
-// import CandidateSelectorProps from '../@types/Candidate';
+import CandidateSelectorProps, { SelectorMode } from '../@types/CandidateSelectorProps';
 
 const Th: React.FC<BoxProps> = ({ children, bg }) => (
   <Box textAlign="center" fontFamily="heading" color="white" as="th" bg={bg} py={2}>
@@ -14,16 +14,49 @@ const Tr: React.FC<BoxProps> = ({ children, bg }) => (
   </Box>
 );
 
-const CandidateSelector: React.FC = () => (
-  <Box as="table" w="100%">
-    <Tr bg="pink.700">
-      <Th>รหัสอ้างอิง</Th>
-      <Th>สาขา</Th>
-      <Th>ผู้ตรวจคำถามกลาง</Th>
-      <Th>ผู้ตรวจคำถามสาขา</Th>
-      <Th>สถานะ</Th>
-    </Tr>
+const Td: React.FC<BoxProps> = ({ children, bg }) => (
+  <Box textAlign="center" fontFamily="body" color="black" as="td" bg={bg} py={2}>
+    {children}
   </Box>
 );
+
+const displayMode = (mode: SelectorMode): string => {
+  if (mode === SelectorMode.Grading) {
+    return 'ให้คะแนน';
+  }
+  if (mode === SelectorMode.Selecting) {
+    return 'คัดเลือกผู้สมัคร';
+  }
+  return 'ตรวจสอบประวัติ';
+};
+
+const CandidateSelector: React.FC<CandidateSelectorProps> = ({ mode, candidates }) => {
+  return (
+    <Box as="table" w="100%">
+      <Tr bg="pink.700">
+        <Th>รหัสอ้างอิง</Th>
+        <Th>สาขา</Th>
+        <Th>ผู้ตรวจคำถามกลาง</Th>
+        <Th>ผู้ตรวจคำถามสาขา</Th>
+        <Th>สถานะ</Th>
+        <Th>{displayMode(mode)}</Th>
+      </Tr>
+      {candidates.map(candidate => {
+        return (
+          <Tr bg="pink.100" key={candidate.id}>
+            <Td>{candidate.id}</Td>
+            <Td>{candidate.track}</Td>
+            <Td>{candidate.graders.general}</Td>
+            <Td>{candidate.graders.track}</Td>
+            <Td>{candidate.status}</Td>
+            <Td>
+              <Button variantColor="blue">{displayMode(mode)}</Button>
+            </Td>
+          </Tr>
+        );
+      })}
+    </Box>
+  );
+};
 
 export default CandidateSelector;
