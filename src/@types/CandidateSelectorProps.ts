ee987ce516e961450be7193d8b7000abfce33d57
pontakornth <pontakorn61@gmail.com>
2020-02-13 09:34:55 +0700

    Add mode to candidate selector

--- src/@types/CandidateSelectorProps.ts
+++ src/@types/CandidateSelectorProps.ts
@@ -1,7 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Candidate from './Candidate';
 
+export enum SelectorMode {
+  Grading,
+  Selecting,
+}
+
 interface CandidateSelectorProps {
   candidates: Array<Candidate>;
-  filter: any;
+  filter?: any;
+  mode: SelectorMode;
 }
+
+export default CandidateSelectorProps;
