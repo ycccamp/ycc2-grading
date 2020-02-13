e58293c2cfc8370565f386eef7ee11b7ce5ebfbf
pontakornth <pontakorn61@gmail.com>
2020-02-13 11:21:39 +0700

    Change name and modify ui

--- src/pages/grading/general/[id].tsx
+++ src/pages/grading/general/[id].tsx
@@ -1,16 +1,16 @@
-import { Heading, Box, Text, Stack, Textarea } from '@chakra-ui/core';
+import { Heading, Box, Text, Stack, Textarea, FormControl, Flex, FormLabel, Input } from '@chakra-ui/core';
 import { useRouter } from 'next/router';
 import { NextPage } from 'next';
 import Layout from '../../../components/Layout';
 import QUESTIONS from '../../../constants/questions';
 
-const PersonGrading: NextPage = () => {
+const PersonGeneralGrading: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
   return (
     <Layout>
       <Heading size="2xl">{`ให้คะแนนผู้สมัครรหัส : ${id}`}</Heading>
-      <Box w="100%" mb={6}>
+      <Box w="100%">
         <Stack spacing={8} py={4}>
           <Heading size="lg">{QUESTIONS.general.Q1}</Heading>
           <Text>ksdvgjspgjpgjpsgj</Text>
@@ -18,18 +18,29 @@ const PersonGrading: NextPage = () => {
           <Text>ksdvgjspgjpgjpsgj</Text>
         </Stack>
       </Box>
-      <Box w="100%">
-        <Heading size="lg">ความคิดเห็น</Heading>
-        <Textarea width={1 / 2} placeholder="ความคิดเห็นของกรรมการ" />
-        <Stack spacing={8}>
-          <Box py={4} alignItems="baseline">
-            <Heading size="md">พู</Heading>
-            <Text>น้องเก่งด้านการให้เหตุผลพอสมควร</Text>
-          </Box>
-        </Stack>
-      </Box>
+      <Flex alignItems="baseline" flexWrap="wrap" width="100%">
+        <Box p={3} mb={6} width="60%">
+          <Heading mb={2} size="lg">
+            ความคิดเห็น
+          </Heading>
+          <Textarea width="100%" placeholder="ความคิดเห็นของกรรมการ" />
+          <Stack width="100%" spacing={4}>
+            <Box py={4} alignItems="baseline">
+              <Heading size="md">พู</Heading>
+              <Text>น้องเก่งด้านการให้เหตุผลพอสมควร</Text>
+            </Box>
+          </Stack>
+        </Box>
+        <Box p={3} w="40%">
+          <Heading size="lg">ให้คะแนน</Heading>
+          <FormControl>
+            <FormLabel>คะแนน</FormLabel>
+            <Input type="number" />
+          </FormControl>
+        </Box>
+      </Flex>
     </Layout>
   );
 };
 
-export default PersonGrading;
+export default PersonGeneralGrading;
