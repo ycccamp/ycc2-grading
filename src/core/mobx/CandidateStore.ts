cb3b9b9da04972cc15aa671dc61a721b5a347ac0
pontakornth <pontakorn61@gmail.com>
2020-02-18 15:18:33 +0700

    Add fetch candidate method

--- src/core/mobx/CandidateStore.ts
+++ src/core/mobx/CandidateStore.ts
@@ -1,8 +1,53 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
 import Candidate from '../../@types/Candidate';
+import RootStore from './RootStore';
+import TRACKS from '../../constants/tracks';
 
 class CandidatesStore {
+  rootStore: RootStore;
+
   @observable candidates: Array<Candidate> = [];
+
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+  }
+
+  @action fetchCandidate(): void {
+    this.rootStore.db
+      .collection('registration')
+      .where('isLocked', '==', false)
+      .get()
+      .then(snapshot => {
+        if (!snapshot.empty) {
+          snapshot.forEach(doc => {
+            this.candidates.push({
+              id: doc.id,
+              track: doc.get('track'),
+              gradingData: {
+                general: {
+                  answers: {
+                    Q1: doc.get('forms.general.Q1') as string,
+                    Q2: doc.get('forms.general.Q2') as string,
+                    Q3: doc.get('forms.general.Q3') as string,
+                  },
+                  score: doc.get('grading.general.score') as number,
+                  grader: doc.get('grading.general.grader') as string,
+                },
+                track: {
+                  answers: {
+                    Q1: doc.get('forms.track.Q1') as string,
+                    Q2: doc.get('forms.track.Q2') as string,
+                  },
+                  score: doc.get('grading.track.score') as number,
+                  grader: doc.get('grading.track.grader') as string,
+                },
+              },
+              status: doc.get('grading.status') as string,
+            });
+          });
+        }
+      });
+  }
 }
 
 export default CandidatesStore;
