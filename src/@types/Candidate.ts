a2f8ceef1b27f542427b25aa7bf6cff47def3d8e
pontakornth <pontakorn61@gmail.com>
2020-02-18 15:17:08 +0700

    Update candidate type to store answer

--- src/@types/Candidate.ts
+++ src/@types/Candidate.ts
@@ -6,10 +6,19 @@ interface Candidate {
   status: string;
   gradingData: {
     general: {
+      answers: {
+        Q1: string;
+        Q2: string;
+        Q3: string;
+      };
       score: number;
       grader: string;
     };
     track: {
+      answers: {
+        Q1: string;
+        Q2: string;
+      };
       score: number;
       grader: string;
     };
