92dd8313b52a626ee4f524ddeb8f99b3e7e1f017
pontakornth <pontakorn61@gmail.com>
2020-02-19 19:28:14 +0700

    Prepare for real data

--- src/pages/select.tsx
+++ src/pages/select.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
+import { useEffect } from 'react';
 import CandidateView from '../components/CandidateView';
 import { SelectorMode } from '../@types/CandidateSelectorProps';
+import { useStore } from '../components/StoreProvider';
 
-const Select: NextPage = () => <CandidateView title="ให้คะแนน" mode={SelectorMode.Selecting} />;
+const Select: NextPage = () => {
+  const store = useStore();
+  return <CandidateView title="ให้คะแนน" mode={SelectorMode.Selecting} candidates={store.candidateStore.candidates} />;
+};
 
 export default Select;
