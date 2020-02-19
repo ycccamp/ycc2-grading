e46825e102011289933605be501b198025833262
pontakornth <pontakorn61@gmail.com>
2020-02-19 15:50:33 +0700

    Make firebase initialize once

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,48 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 import { ThemeProvider } from 'emotion-theming';
 import { CSSReset } from '@chakra-ui/core';
 import { Global, css } from '@emotion/core';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import firebaseConfig from '../constants/firebase.config';
 import theme from '../theme';
-// import StoreProvider from '../components/StoreProvider';
+import StoreProvider from '../components/StoreProvider';
 
-const App = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>YCC Grading</title>
-      <link href="https://fonts.googleapis.com/css?family=Prompt|Sarabun&display=swap" rel="stylesheet" />
-    </Head>
-    <ThemeProvider theme={theme}>
-      <CSSReset />
-      <Global
-        styles={css`
-          html,
-          body {
-            height: 100%;
-          }
-          #__next {
-            height: 100%;
-          }
-        `}
-      />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </>
-);
+const App = ({ Component, pageProps }) => {
+  const [db, setDB] = useState(null);
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+    setDB(firebase.firestore());
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>YCC Grading</title>
+        <link href="https://fonts.googleapis.com/css?family=Prompt|Sarabun&display=swap" rel="stylesheet" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Global
+          styles={css`
+            html,
+            body {
+              height: 100%;
+            }
+            #__next {
+              height: 100%;
+            }
+          `}
+        />
+        <StoreProvider firebaseDB={db}>
+          <Component {...pageProps} />
+        </StoreProvider>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
