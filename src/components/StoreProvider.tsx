69cbb735ae6e89481055b61ec37e01924d407534
pontakornth <pontakorn61@gmail.com>
2020-02-19 10:28:09 +0700

    Make store provider

--- src/components/StoreProvider.tsx
+++ src/components/StoreProvider.tsx
@@ -1,7 +1,28 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import React from 'react';
+import { useLocalStore } from 'mobx-react';
 import firebaseConfig from '../constants/firebase.config';
+import RootStore from '../core/mobx/RootStore';
 
 firebase.initializeApp(firebaseConfig);
 
-// const db = firebase.firestore();
+const db = firebase.firestore();
+
+const storeContext = React.createContext(null);
+
+const rootStore = new RootStore(db);
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const StoreProvider = ({ children }) => {
+  const store = useLocalStore(() => rootStore);
+  return <storeContext.Provider value={store}>{children}</storeContext.Provider>;
+};
+
+export const useStore = (): RootStore => {
+  const store = React.useContext(storeContext);
+  if (!store) {
+    throw new Error('useStore must be used within a StoreProvider');
+  }
+  return store;
+};
