b95576e30e7ad132e40cccf67aa0d592d4a19a99
pontakornth <pontakorn61@gmail.com>
2020-02-19 15:25:00 +0700

    Fix store error

--- src/components/StoreProvider.tsx
+++ src/components/StoreProvider.tsx
@@ -1,19 +1,17 @@
-import * as firebase from 'firebase';
-import 'firebase/firestore';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocalStore } from 'mobx-react';
-import firebaseConfig from '../constants/firebase.config';
+import { observable } from 'mobx';
 import RootStore from '../core/mobx/RootStore';
 
 const storeContext = React.createContext(null);
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const StoreProvider = ({ children }) => {
-  const store = useLocalStore(() => new RootStore());
+const StoreProvider = ({ children, firebaseDB }) => {
+  const rootStore = new RootStore(firebaseDB);
+  useEffect(() => {
+    rootStore.init();
+  }, []);
+  const store = useLocalStore(() => observable.box(rootStore));
   return <storeContext.Provider value={store}>{children}</storeContext.Provider>;
 };
 
