b3ec2cd8afd764ae79b346825b6420a8c010aa2f
pontakornth <pontakorn61@gmail.com>
2020-02-22 20:27:22 +0700

    Add role check

--- src/pages/grading/track/[id]/index.tsx
+++ src/pages/grading/track/[id]/index.tsx
@@ -1,45 +1,14 @@
-import { useRouter } from 'next/router';
 import { NextPage } from 'next';
 import CandidateGradingView from '../../../../components/CandidateGradingView';
 import { GradingMode } from '../../../../@types/CandidateGradingViewProps';
-import Candidate from '../../../../@types/Candidate';
-
-const mockCandidates: Array<Candidate> = [
-  {
-    id: 'njdfgndfonjdfobn',
-    track: 'creative',
-    status: 'กำลังตรวจ',
-    gradingData: {
-      general: {
-        answers: {
-          Q1: '',
-          Q2: '',
-          Q3: '',
-        },
-        score: {
-          Q1: 8.0,
-          Q2: 8.0,
-          Q3: 8.0,
-        },
-        grader: 'พล',
-      },
-      track: {
-        answers: {
-          Q1: '',
-          Q2: '',
-        },
-        score: {
-          Q1: 7.0,
-          Q2: 8.0,
-        },
-        grader: 'อิ้ง',
-      },
-    },
-  },
-];
+import Authorization from '../../../../components/Authorization';
 
 const PersonTrackGrading: NextPage = () => {
-  return <CandidateGradingView mode={GradingMode.Track} candidate={mockCandidates[0]} />;
+  return (
+    <Authorization accessibleRoles={['admin', 'creative', 'developer', 'design']}>
+      <CandidateGradingView mode={GradingMode.Track} />
+    </Authorization>
+  );
 };
 
 export default PersonTrackGrading;
