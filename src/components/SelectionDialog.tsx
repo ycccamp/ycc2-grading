69281f86f5df614213043e3fa444422ec7d3b15f
pontakornth <pontakorn61@gmail.com>
2020-02-26 17:02:02 +0700

    Make selection usable

--- src/components/SelectionDialog.tsx
+++ src/components/SelectionDialog.tsx
@@ -8,27 +8,29 @@ import {
   Button,
 } from '@chakra-ui/core';
 import SelectionDialogProps from '../@types/SelectionDialogProps';
+import { useStore } from './StoreProvider';
 
-const SelectionDialog: React.FC<SelectionDialogProps> = ({ isOpen, id, onClose, onSelect, onAlternate, onDelist }) => {
+const SelectionDialog: React.FC<SelectionDialogProps> = ({ isOpen, id, onClose }) => {
+  const { candidateStore } = useStore();
   const cancelRef = useRef();
   return (
-    <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+    <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={(): void => onClose}>
       <AlertDialogOverlay />
       <AlertDialogContent>
         <AlertDialogHeader fontSize="lg" fontWeight="bold" fontFamily="heading">
           {`คัดเลือกผู้สมัครรหัส ${id}`}
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <Button variantColor="green" onClick={onSelect}>
+          <Button variantColor="green" onClick={(): void => candidateStore.selectCandidate(id, 'selected')}>
             ตัวจริง
           </Button>
-          <Button variantColor="yellow" onClick={onAlternate}>
+          <Button variantColor="yellow" onClick={(): void => candidateStore.selectCandidate(id, 'alternate')}>
             ตัวสำรอง
           </Button>
-          <Button variantColor="red" onClick={onDelist}>
+          <Button variantColor="red" onClick={(): void => candidateStore.selectCandidate(id, 'delisted')}>
             คัดออก
           </Button>
-          <Button ref={cancelRef} variantColor="red" variant="outline" onClick={onClose}>
+          <Button ref={cancelRef} variantColor="red" variant="outline" onClick={(): void => onClose()}>
             ยกเลิก
           </Button>
         </AlertDialogFooter>
