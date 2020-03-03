a0b6443f851a7219f016d256b57fb71a23bf5652
pontakornth <pontakorn61@gmail.com>
2020-03-03 14:34:34 +0700

    Simplify candidate store

--- src/core/mobx/CandidateStore.ts
+++ src/core/mobx/CandidateStore.ts
@@ -9,6 +9,7 @@ import TRACKS from '../../constants/tracks';
 import firebase from '../../constants/firebase';
 import { getAverageScore } from '../utils';
 import { GradingMode } from '../../@types/CandidateGradingViewProps';
+import { docToCandidate } from '../candidate_utils';
 
 const db = firebase().firestore();
 
@@ -43,78 +44,9 @@ class CandidatesStore {
         if (!snapshot.empty) {
           snapshot.forEach(async doc => {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const formData: any = {};
-            const generalSnapshot = await db
-              .collection('registration')
-              .doc(doc.id)
-              .collection('forms')
-              .doc('general')
-              .get();
-            formData.general = {
-              Q1: generalSnapshot.get('Q1'),
-              Q2: generalSnapshot.get('Q2'),
-              Q3: generalSnapshot.get('Q3'),
-            };
-            const trackSnapshot = await db
-              .collection('registration')
-              .doc(doc.id)
-              .collection('forms')
-              .doc('track')
-              .get();
-            formData.track = {
-              Q1: trackSnapshot.get('Q1') || trackSnapshot.get('Q1__Upload'),
-              Q2: trackSnapshot.get('Q2'),
-              Q3: trackSnapshot.get('Q3'),
-            };
+            const candidate = await docToCandidate(doc);
 
-            const generalScoreSnapshot = await db
-              .collection('registration')
-              .doc(doc.id)
-              .collection('grading')
-              .doc('general')
-              .collection('score')
-              .get();
-
-            const trackScoreSnapshot = await db
-              .collection('registration')
-              .doc(doc.id)
-              .collection('grading')
-              .doc('track')
-              .collection('score')
-              .get();
-
-            this.candidates.push({
-              id: doc.id,
-              track: doc.get('track'),
-              gradingData: {
-                general: {
-                  answers: {
-                    Q1: formData.general.Q1 as string,
-                    Q2: formData.general.Q2 as string,
-                    Q3: formData.general.Q3 as string,
-                  },
-                  score: generalScoreSnapshot.docs.map(score => ({
-                    grader: score.id,
-                    Q1: score.get('Q1'),
-                    Q2: score.get('Q2'),
-                    Q3: score.get('Q3'),
-                  })),
-                },
-                track: {
-                  answers: {
-                    Q1: formData.track.Q1 as string,
-                    Q2: formData.track.Q2 as string,
-                  },
-                  score: trackScoreSnapshot.docs.map(score => ({
-                    grader: score.id,
-                    Q1: score.get('Q1'),
-                    Q2: score.get('Q2'),
-                  })),
-                },
-              },
-              status: (doc.get('status') as string) || 'ยังตรวจไม่เสร็จ',
-              timestamp: doc.get('timestamp') as number,
-            });
+            this.candidates.push(candidate);
           });
         }
         this.isLoaded = true;
